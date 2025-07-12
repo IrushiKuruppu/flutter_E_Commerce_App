fc0c3d903ac141552adef1df60caa0954f10aec0
irushi kuruppu <irushikuruppu1996@gmail.com>
2025-07-12 23:19:31 +0530

    folder structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:e_commerce_app/utils/themes/themes.dart';
+import 'package:e_commerce_app/utils/themes/themes.dart ';
 
 void main() {
   runApp(const MyApp());
